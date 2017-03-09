5a5b09e952492f1e1b032712f14b9c3a61754d63
Mittal Bhanderi <Mittal Bhanderi>
2017-03-09 09:56:15 +0000

    Address Processing Solution Added

---
+++ src/AddressProcessor.Tests/CSVReaderWriterTests.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using AddressProcessing.CSV;
+using NUnit.Framework;
+using System.IO;
+
+namespace AddressProcessing.Tests
+{
+    [TestFixture]
+    public class CSVReaderWriterTests
+    {
+        private const string TestInputFile = @"test_data\contacts.csv";
+        private const string TestOutputFile = @"test_data\contacts-out.csv";
+
+        [Test]
+        public void Open_Method_Should_throw_Exception()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+           Assert.Throws<Exception>(() =>csvReaderWriter.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestInputFile), (CSVReaderWriter.Mode)3));
+
+        }
+
+        [Test]
+        public void Open_Method_Should_throw_IOException()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            Assert.Throws<IOException>(() => csvReaderWriter.Open(TestInputFile, CSVReaderWriter.Mode.Read));
+
+        }
+
+        [Test]
+        public void Open_Method_to_read_Should_not_throw_Exception()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            Assert.DoesNotThrow(() => csvReaderWriter.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestInputFile), CSVReaderWriter.Mode.Read));
+
+            csvReaderWriter.Close();
+        }
+
+        [Test]
+        public void Open_Method_to_write_Should_not_throw_Exception()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            Assert.DoesNotThrow(() => csvReaderWriter.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestOutputFile), CSVReaderWriter.Mode.Write));
+
+            csvReaderWriter.Close();
+        }
+
+        [Test]
+        public void Read_Method_Should_read_first_line_of_file()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            csvReaderWriter.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestInputFile), CSVReaderWriter.Mode.Read);
+
+            string column1, column2;
+            csvReaderWriter.Read(out column1, out column2);
+
+            csvReaderWriter.Close();
+
+            Assert.IsTrue(string.Equals(column1, "Shelby Macias"));
+            Assert.IsTrue(string.Equals(column2, "3027 Lorem St.|Kokomo|Hertfordshire|L9T 3D5|England"));
+        }
+
+        [Test]
+        public void Read_Method_Should_return_false_with_null_ref_input_string_parameters()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+            string column1, column2;
+            var result = csvReaderWriter.Read(out column1, out column2);
+            Assert.False(result);
+            Assert.Null(column1);
+            Assert.Null(column2);
+        }
+
+        [Test]
+        public void Read_Method_Should_read_till_end_of_file()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            csvReaderWriter.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestInputFile), CSVReaderWriter.Mode.Read);
+
+            string column1, column2;
+            int counter = 0;
+
+            while (csvReaderWriter.Read(out column1, out column2))
+            {
+                counter++;
+            }
+
+            csvReaderWriter.Close();
+
+            Assert.That(counter, Is.EqualTo(229));
+        }
+
+        [Test]
+        public void Write_Method_Should_not_throw_Exception_when_empty_string_array_is_passed()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            Assert.DoesNotThrow(() => csvReaderWriter.Write(new string[] {}));
+
+            csvReaderWriter.Close();
+        }
+
+        [Test]
+        public void Write_Method_Should_not_throw_Exception_when_non_empty_string_array_is_passed()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            Assert.DoesNotThrow(() => csvReaderWriter.Write(new string[] { "test", "data" }));
+
+            csvReaderWriter.Close();
+        }
+
+        [Test]
+        public void Write_Method_Should_write_to_the_file()
+        {
+            var csvReaderWriter = new CSVReaderWriter();
+
+            csvReaderWriter.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestOutputFile), CSVReaderWriter.Mode.Write);
+            
+            csvReaderWriter.Write(new string[] {"This", "should", "be", "written", "to", "the", "file"});
+
+            csvReaderWriter.Close();
+
+            Assert.True(File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestOutputFile)));
+            File.Delete(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestOutputFile));
+        }
+
+        [Test]
+        public void Read_and_Write_Methods_Should_read_and_write_file()
+        {
+            var reader = new CSVReaderWriter();
+            var writer = new CSVReaderWriter();
+            int counter = 0;
+
+            reader.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestInputFile), CSVReaderWriter.Mode.Read);
+            writer.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestOutputFile), CSVReaderWriter.Mode.Write);
+
+            string column1, column2;
+            while (reader.Read(out column1, out column2))
+            {
+                writer.Write(new string[] { column1, column2 });
+                counter++;
+            }
+
+            reader.Close();
+            writer.Close();
+
+            Assert.That(counter, Is.EqualTo(229));
+            Assert.True(File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestOutputFile)));
+            File.Delete(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TestOutputFile));
+
+        }
+    }
+}
---
+++ src/AddressProcessor/CSV/CSVReaderWriter.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AddressProcessing.CSV
+{
+    /*
+        2) Refactor this class into clean, elegant, rock-solid & well performing code, without over-engineering.
+           Assume this code is in production and backwards compatibility must be maintained.
+    */
+
+    public class CSVReaderWriter
+    {
+        private StreamReader _readerStream = null;
+        private StreamWriter _writerStream = null;
+
+        //'[Flags]' attribute removed - File can either be in a read mode or in write mode
+        public enum Mode { Read = 1, Write = 2 };
+
+        public void Open(string fileName, Mode mode)
+        {
+            try
+            {
+                if (mode == Mode.Read)
+                {
+                    //StreamReader is lightweight and more efficient compared to File.OpenText
+                    _readerStream = new StreamReader(fileName, true);
+                }
+                else if (mode == Mode.Write)
+                {
+                    //StreamWriter is the direct instance of CreateText() method without initializing FileInfo class 
+                    _writerStream = new StreamWriter(fileName);
+                }
+                else
+                {
+                    throw new Exception("Unknown file mode for " + fileName);
+                }
+            }
+            // catch and throw IOException in case the file could be found.
+            catch (IOException ioex)
+            {
+                throw new IOException($"Error opening/locating the file {ioex.Message}");
+            }
+        }
+
+        public void Write(params string[] columns)
+        {
+            // Comment the if condition below to write empty string line to the file
+            if (columns.Length > 0)
+            {
+                // string variable replaced with Strigbuilder for better performance
+                StringBuilder outPut = new StringBuilder();
+
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    outPut.Append(columns[i]);
+                    if ((columns.Length - 1) != i)
+                    {
+                        outPut.Append("\t");
+                    }
+                }
+
+                WriteLine(outPut.ToString());
+            }
+        }
+
+        public bool Read(out string column1, out string column2)
+        {
+            string line = ReadLine();
+
+            if (line == null)
+            {
+                // If there is a blank like in between ignore it and read the next line
+                return ReadNextLine(out column1, out column2);
+            }
+            // We just need a char '\t' rather a char array..
+            string[] columns = line.Split('\t');
+
+            if (columns.Length < 2)
+            {
+                // If there is a line with just one column (tab group) then ignore it and read the next line
+                return ReadNextLine(out column1, out column2);
+            }
+
+            // line read is valid - set values and return
+            // additional conditions required if we want the values to be checked for null or empty!
+            column1 = columns[0];
+            column2 = columns[1];
+
+            return true;
+        }
+
+        private void WriteLine(string line)
+        {
+            // make sure that _writeStream is not null
+            _writerStream?.WriteLine(line);
+        }
+
+        private string ReadLine()
+        {
+            // make sure that _readStream is not null
+            return _readerStream?.ReadLine();
+        }
+
+        private bool ReadNextLine(out string column1, out string column2)
+        {
+            // Check if it is end of file
+            if (_readerStream != null && !_readerStream.EndOfStream)
+            {
+                // continue reading
+                return Read(out column1, out column2);
+            }
+            // We have reached end of file 
+            column1 = null;
+            column2 = null;
+            return false;
+        }
+
+        public void Close()
+        {
+            // C# 6.0 - Usage of null conditional operator
+            _writerStream?.Close();
+            _readerStream?.Close();
+        }
+    }
+}
